08a25ee809569716bc601a11faeda742068022db
paobritto <163893644+paobritto@users.noreply.github.com>
2026-04-02 11:40:59 -0300

    Delete AMST4.Carousel.MVC directory

--- AMST4.Carousel.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AMST4.Carousel.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
